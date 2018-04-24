bbfdf51c2ca6ebe4564e882d2be3c0fe9010803d
Erwan SEVELLEC <erwan@sevellec.net>
2018-04-25 00:37:33 +0200

    add test reviewItemBookTest

--- src/tests/AddItemBookTest.java
+++ src/tests/AddItemBookTest.java
@@ -9,12 +9,11 @@ import exceptions.NotTestReportException;
 import exceptions.ItemBookAlreadyExistsException;
 
 public class AddItemBookTest {
-	
+
 	/**
-	 * Check that trying to add this new item book (login, password, title,
-			kind, author, nbPages) raises a
-	 * BadEntry exception and does not change content of the
-	 * <i>ISocialNetwork</i>. If OK, the method just returns 0. If not OK,
+	 * Check that trying to add this new item book (login, password, title, kind,
+	 * author, nbPages) raises a BadEntry exception and does not change content of
+	 * the <i>ISocialNetwork</i>. If OK, the method just returns 0. If not OK,
 	 * displays an error message and returns 1.
 	 * 
 	 * @param sn
@@ -26,14 +25,14 @@ public class AddItemBookTest {
 	 * @param title
 	 *            - new item book's title
 	 * @param kind
-	 * 			  - new item book's kind
+	 *            - new item book's kind
 	 * @param author
-	 * 			  - new item book's author
+	 *            - new item book's author
 	 * @param nbPages
-	 * 			  - new item book's number of pages
+	 *            - new item book's number of pages
 	 * @param testId
-	 *            - the test ID that will prefix any error message displayed by
-	 *            this method
+	 *            - the test ID that will prefix any error message displayed by this
+	 *            method
 	 * @param errorMessage
 	 *            - the error message that will be displayed if no exception is
 	 *            thrown when adding this member
@@ -47,25 +46,23 @@ public class AddItemBookTest {
 			System.out.println("Err " + testId + " : " + errorMessage);
 			return 1;
 		} catch (BadEntryException e) {
-			if (sn.nbMembers() != nbItemBooks) {	
-				System.out.println("Err "+ testId + " : BadEntry was thrown but the number of item books was changed");
+			if (sn.nbMembers() != nbItemBooks) {
+				System.out.println("Err " + testId + " : BadEntry was thrown but the number of item books was changed");
 				return 1;
 			} else
 				return 0;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Err " + testId + " : unexpected exception. " + e);
 			e.printStackTrace();
 			return 1;
 		}
 	}
-	
+
 	/**
-	 * Check that trying to add this new item book (login, password, title,
-			kind, author, nbPages) raises a
-	 * NotMemberException exception and does not change content of the
-	 * <i>ISocialNetwork</i>. If OK, the method just returns 0. If not OK,
-	 * displays an error message and returns 1.
+	 * Check that trying to add this new item book (login, password, title, kind,
+	 * author, nbPages) raises a NotMemberException exception and does not change
+	 * content of the <i>ISocialNetwork</i>. If OK, the method just returns 0. If
+	 * not OK, displays an error message and returns 1.
 	 * 
 	 * @param sn
 	 *            - the <i>ISocialNetwork</i>
@@ -76,14 +73,14 @@ public class AddItemBookTest {
 	 * @param title
 	 *            - new item book's title
 	 * @param kind
-	 * 			  - new item book's kind
+	 *            - new item book's kind
 	 * @param author
-	 * 			  - new item book's author
+	 *            - new item book's author
 	 * @param nbPages
-	 * 			  - new item book's number of pages
+	 *            - new item book's number of pages
 	 * @param testId
-	 *            - the test ID that will prefix any error message displayed by
-	 *            this method
+	 *            - the test ID that will prefix any error message displayed by this
+	 *            method
 	 * @param errorMessage
 	 *            - the error message that will be displayed if no exception is
 	 *            thrown when adding this member
@@ -92,14 +89,15 @@ public class AddItemBookTest {
 	private static int addItemBookNotMemberExceptionTest(ISocialNetwork sn, String login, String password, String title,
 			String kind, String author, int nbPages, String testId, String errorMessage) {
 		int nbItemsBooks = sn.nbBooks();
-		
+
 		try {
 			sn.addItemBook(login, password, title, kind, author, nbPages);
 			System.out.println("Err " + testId + " : " + errorMessage);
 			return 1;
 		} catch (NotMemberException e) {
-			if(nbItemsBooks != sn.nbBooks()) {
-				System.out.println("Err "+ testId + " : NotMemberException was thrown but the number of item books was changed");
+			if (nbItemsBooks != sn.nbBooks()) {
+				System.out.println(
+						"Err " + testId + " : NotMemberException was thrown but the number of item books was changed");
 				return 1;
 			} else {
 				return 0;
@@ -110,13 +108,12 @@ public class AddItemBookTest {
 			return 1;
 		}
 	}
-	
+
 	/**
-	 * Check that trying to add this new item book (login, password, title,
-			kind, author, nbPages) raises a
-	 * ItemBookAlreadyExistsException exception and does not change content of the
-	 * <i>ISocialNetwork</i>. If OK, the method just returns 0. If not OK,
-	 * displays an error message and returns 1.
+	 * Check that trying to add this new item book (login, password, title, kind,
+	 * author, nbPages) raises a ItemBookAlreadyExistsException exception and does
+	 * not change content of the <i>ISocialNetwork</i>. If OK, the method just
+	 * returns 0. If not OK, displays an error message and returns 1.
 	 * 
 	 * @param sn
 	 *            - the <i>ISocialNetwork</i>
@@ -127,30 +124,31 @@ public class AddItemBookTest {
 	 * @param title
 	 *            - new item book's title
 	 * @param kind
-	 * 			  - new item book's kind
+	 *            - new item book's kind
 	 * @param author
-	 * 			  - new item book's author
+	 *            - new item book's author
 	 * @param nbPages
-	 * 			  - new item book's number of pages
+	 *            - new item book's number of pages
 	 * @param testId
-	 *            - the test ID that will prefix any error message displayed by
-	 *            this method
+	 *            - the test ID that will prefix any error message displayed by this
+	 *            method
 	 * @param errorMessage
 	 *            - the error message that will be displayed if no exception is
 	 *            thrown when adding this member
 	 * @return 0 if the test is OK, 1 if not
 	 */
-	private static int addItemItemBookAleadyExistsException(ISocialNetwork sn, String login, String password, String title,
-			String kind, String author, int nbPages, String testId, String errorMessage) {
+	private static int addItemItemBookAleadyExistsException(ISocialNetwork sn, String login, String password,
+			String title, String kind, String author, int nbPages, String testId, String errorMessage) {
 		int nbItemsBooks = sn.nbBooks();
-		
+
 		try {
 			sn.addItemBook(login, password, title, kind, author, nbPages);
 			System.out.println("Err " + testId + " : " + errorMessage);
 			return 1;
 		} catch (ItemBookAlreadyExistsException e) {
-			if(nbItemsBooks != sn.nbBooks()) {
-				System.out.println("Err "+ testId + " : ItemBookAlreadyExistsException was thrown but the number of item books was changed");
+			if (nbItemsBooks != sn.nbBooks()) {
+				System.out.println("Err " + testId
+						+ " : ItemBookAlreadyExistsException was thrown but the number of item books was changed");
 				return 1;
 			} else {
 				return 0;
@@ -161,9 +159,9 @@ public class AddItemBookTest {
 			return 1;
 		}
 	}
-	
+
 	/**
-	  * Check that a new item book can be added if parameter are correct
+	 * Check that a new item book can be added if parameter are correct
 	 * 
 	 * @param sn
 	 *            - the <i>ISocialNetwork</i>
@@ -174,26 +172,27 @@ public class AddItemBookTest {
 	 * @param title
 	 *            - new item book's title
 	 * @param kind
-	 * 			  - new item book's kind
+	 *            - new item book's kind
 	 * @param author
-	 * 			  - new item book's author
+	 *            - new item book's author
 	 * @param nbPages
-	 * 			  - new item book's number of pages
+	 *            - new item book's number of pages
 	 * @param testId
-	 *            - the test ID that will prefix any error message displayed by
-	 *            this method
+	 *            - the test ID that will prefix any error message displayed by this
+	 *            method
 	 * @param errorMessage
 	 *            - the error message that will be displayed if no exception is
 	 *            thrown when adding this member
 	 * @return 0 if the test is OK, 1 if not
 	 */
-	private static int addItemBookOKTest(ISocialNetwork sn, String login, String password, String title,
-			String kind, String author, int nbPages, String testId) {
+	private static int addItemBookOKTest(ISocialNetwork sn, String login, String password, String title, String kind,
+			String author, int nbPages, String testId) {
 		int nbItemBooks = sn.nbBooks();
 		try {
 			sn.addItemBook(login, password, title, kind, author, nbPages);
 			if (nbItemBooks + 1 != sn.nbBooks()) {
-				System.out.println("Err " + testId + " : the number of item books (" + nbItemBooks + ") was not incremented");
+				System.out.println(
+						"Err " + testId + " : the number of item books (" + nbItemBooks + ") was not incremented");
 				return 1;
 			} else {
 				return 0;
@@ -204,104 +203,128 @@ public class AddItemBookTest {
 			return 1;
 		}
 	}
-	
+
 	public static TestReport test() {
 		ISocialNetwork sn = new SocialNetwork();
-		
+
 		try {
 			sn.addMember("esevellec", "1234", "Lecteur assidu");
 		} catch (Exception e) {
-			System.out.println("addMember does not working");
+			System.out.println("Err : addMember() throw an exception");
 			e.printStackTrace();
 		}
-		
-		int nbBooks = sn.nbBooks();
+
 		int nbFilms = sn.nbFilms();
-		
+		int nbMembers = sn.nbMembers();
+
 		int nbTests = 0;
 		int nbErrors = 0;
-		
+
 		System.out.print("Testing AddItemBook()");
-		
-		//Test n°3
+
+		// Test n°3
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, null, "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "3.1", "addItemBook() doesn't reject null logins");
-		
+		nbErrors += addItemBookBadEntryTest(sn, null, "1234", "Lord Of The Rings", "heroic fantasy", "J. R. R. Tolkien",
+				30, "3.1", "addItemBook() doesn't reject null login");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, " ", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "3.2", "addItemBook() doesn't reject logins that don't contain at least one character other than space");
-		
+		nbErrors += addItemBookBadEntryTest(sn, " ", "1234", "Lord Of The Rings", "heroic fantasy", "J. R. R. Tolkien",
+				30, "3.2",
+				"addItemBook() doesn't reject logins that don't contain at least one character other than space");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", null, "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "3.3", "addItemBook() doesn't reject null password");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", null, "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "3.3", "addItemBook() doesn't reject null password");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "    123 ", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "3.4", "addItemBook() doesn't reject that don't contain at least 4 characters (not taking into account leading or trailing blanks)");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "    123 ", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "3.4",
+				"addItemBook() doesn't reject that don't contain at least 4 characters (not taking into account leading or trailing blanks)");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", null, "heroic fantasy", "J. R. R. Tolkien", 30, "3.5", "addItemBook() doesn't reject null title");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", null, "heroic fantasy", "J. R. R. Tolkien", 30, "3.5",
+				"addItemBook() doesn't reject null title");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", "     ", "heroic fantasy", "J. R. R. Tolkien", 30, "3.6", "addItemBook() doesn't reject only space title");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "1234", "     ", "heroic fantasy", "J. R. R. Tolkien", 30,
+				"3.6", "addItemBook() doesn't reject only space title");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", "Lord of the ring", null, "J. R. R. Tolkien", 30, "3.7", "addItemBook() doesn't reject null kind");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "1234", "Lord Of The Rings", null, "J. R. R. Tolkien", 30,
+				"3.7", "addItemBook() doesn't reject null kind");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", "Lord of the ring", "heroic fantasy", null, 30, "3.8", "addItemBook() doesn't reject null author");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy", null, 30,
+				"3.8", "addItemBook() doesn't reject null author");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 0, "3.9", "addItemBook() doesn't reject null number of pages");
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 0, "3.9", "addItemBook() doesn't reject null number of pages");
+
 		nbTests++;
-		nbErrors += addItemBookBadEntryTest(sn, "Erwan", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", -5, "3.10", "addItemBook() doesn't reject negative number of pages");
-		
-		//Test n°4		
-		
+		nbErrors += addItemBookBadEntryTest(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", -5, "3.10", "addItemBook() doesn't reject negative number of pages");
+
+		// Test n°4
+
 		nbTests++;
-		nbErrors += addItemBookNotMemberExceptionTest(sn, "Erwan", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "4.1", "addItemBook() doesn't reject a not member login");
-		
+		nbErrors += addItemBookNotMemberExceptionTest(sn, "Erwan", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "4.1", "addItemBook() doesn't reject a bad login");
+
 		nbTests++;
-		nbErrors += addItemBookNotMemberExceptionTest(sn, "esevellec", "12345", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "4.2", "addItemBook() doesn't reject a not matching password");
-		
+		nbErrors += addItemBookNotMemberExceptionTest(sn, "esevellec", "12345", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "4.2", "addItemBook() doesn't reject a bad password");
+
 		nbTests++;
-		nbErrors += addItemBookOKTest(sn, "esevellec", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "4.2");
-		
+		nbErrors += addItemBookOKTest(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "4.3");
+
 		nbTests++;
-		nbErrors += addItemBookOKTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", "fantasy", "Lewis Carroll", 44, "4.2");
-		
+		nbErrors += addItemBookOKTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", "fantasy",
+				"Lewis Carroll", 44, "4.3");
+
 		nbTests++;
-		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Lord of the ring", "heroic fantasy", "J. R. R. Tolkien", 30, "4.2", "addItemBook() doesn't reject a already existing book title");
-		
+		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "4.4", "addItemBook() doesn't reject a already existing book title");
+
 		nbTests++;
-		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", "fantasy", "Lewis Carroll", 44, "4.2", "addItemBook() doesn't reject a already existing book title");
-		
+		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland",
+				"fantasy", "Lewis Carroll", 44, "4.5", "addItemBook() doesn't reject a already existing book title");
+
 		nbTests++;
-		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Lord Of tHe rInG", "heroic fantasy", "J. R. R. Tolkien", 30, "4.2", "addItemBook() doesn't reject a already existing book title with a different case");
-		
+		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "Lord Of The Rings", "heroic fantasy",
+				"J. R. R. Tolkien", 30, "4.6",
+				"addItemBook() doesn't reject a already existing book title with a different case");
+
 		nbTests++;
-		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "   Lord of the ring    ", "heroic fantasy", "J. R. R. Tolkien", 30, "4.2", "addItemBook() doesn't reject a already existing book title without taking into account leading or trailing blanks");
-		
+		nbErrors += addItemItemBookAleadyExistsException(sn, "esevellec", "1234", "   Lord Of The Rings    ",
+				"heroic fantasy", "J. R. R. Tolkien", 30, "4.7",
+				"addItemBook() doesn't reject a already existing book title without taking into account leading or trailing blanks");
+
 		nbTests++;
-		// check that 'sn' was not modified
 		if (nbFilms != sn.nbFilms()) {
-			System.out
-					.println("Error : the number of films was unexepectedly changed by addMember()");
+			System.out.println("Error : the number of films was unexepectedly changed by addItemBook()");
 			nbErrors++;
 		}
-		
+
+		nbTests++;
+		if (nbMembers != sn.nbMembers()) {
+			System.out.println("Error : the number of members was unexepectedly chaged by addItemBook()");
+		}
+
 		// Display final state of 'sn'
 		System.out.println("Final state of the social network : " + sn);
-		
-		try{
-			TestReport tr = new TestReport(nbTests, nbErrors);	
+
+		try {
+			TestReport tr = new TestReport(nbTests, nbErrors);
 			System.out.println("AddItemBook : " + tr);
-			return tr;	
-		}
-		catch (NotTestReportException e){ //This shouldn't happen
-			System.out.println("Unexpected error in AddMemberTest test code - Can't return valuable test results");
+			return tr;
+		} catch (NotTestReportException e) {
+			System.out.println("Unexpected error in AddItemBookTest test code - Can't return valuable test results");
 			return null;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		test();
 	}
---
+++ src/tests/ReviewItemBook.java
@@ -0,0 +1,196 @@
+package tests;
+
+import opinion.ISocialNetwork;
+import opinion.SocialNetwork;
+
+import exceptions.BadEntryException;
+import exceptions.NotMemberException;
+import exceptions.NotTestReportException;
+import exceptions.NotItemException;
+
+public class ReviewItemBook {
+
+	private static int reviewItemBookBadEntryTest(ISocialNetwork sn, String login, String password, String title,
+			float mark, String comment, String testId, String errorMessage) {
+		try {
+			sn.reviewItemBook(login, password, title, mark, comment);
+			System.out.println("Err " + testId + " : " + errorMessage);
+			return 1;
+		} catch (BadEntryException e) {
+			return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+
+	private static int reviewItemBookNotMemberExceptionTest(ISocialNetwork sn, String login, String password,
+			String title, float mark, String comment, String testId, String errorMessage) {
+		try {
+			sn.reviewItemBook(login, password, title, mark, comment);
+			System.out.println("Err" + testId + " : " + errorMessage);
+			return 1;
+		} catch (NotMemberException e) {
+			return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+
+	private static int reviewItemBookNotItemExceptionTest(ISocialNetwork sn, String login, String password,
+			String title, float mark, String comment, String testId, String errorMessage) {
+		try {
+			sn.reviewItemBook(login, password, title, mark, comment);
+			System.out.println("Err" + testId + " : " + errorMessage);
+			return 1;
+		} catch (NotMemberException e) {
+			return 0;
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception. " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+
+	private static int reviewItemBookOKTest(ISocialNetwork sn, String login, String password, String title, float mark,
+			String comment, float expectedMean, String testId) {
+		try {
+			float returnedMark = sn.reviewItemBook(login, password, title, mark, comment);
+			if (returnedMark != expectedMean) {
+				System.out.println(
+						"Err " + testId + " : the mean of the marks returned is not correct (" + returnedMark + ")");
+				return 1;
+			} else {
+				return 0;
+			}
+		} catch (Exception e) {
+			System.out.println("Err " + testId + " : unexpected exception " + e);
+			e.printStackTrace();
+			return 1;
+		}
+	}
+
+	private static TestReport test() {
+		int nbTests = 0;
+		int nbErrors = 0;
+
+		ISocialNetwork sn = new SocialNetwork();
+		try {
+			sn.addMember("esevellec", "1234", "Beginner reader");
+			sn.addMember("kthezelais", "1234", "Confirmed reader");
+		} catch (Exception e) {
+			System.out.println("Err : addMember() throw an exception");
+			e.printStackTrace();
+		}
+
+		try {
+			sn.addItemBook("esevellec", "1234", "Alice’s Adventures in Wonderland", "fantasy", "Lewis Carroll", 44);
+		} catch (Exception e) {
+			System.out.println("addItemBook() throw an exception");
+			e.printStackTrace();
+		}
+
+		int nbBooks = sn.nbBooks();
+		int nbMembers = sn.nbMembers();
+		int nbFilms = sn.nbFilms();
+
+		System.out.println("Testint reviewItemBook()");
+
+		// test 5
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, null, "1234", "Alice’s Adventures in Wonderland", 3, "Great book!",
+				"5.1", "reviewItemBook() doesn't reject null login");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, " ", "1234", "Alice’s Adventures in Wonderland", 3, "Great book!",
+				"5.2",
+				"reviewItemBook() doesn't reject logins that don't contain at least one character other than space");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", null, "Alice’s Adventures in Wonderland", 3,
+				"Great book!", "5.3", "reviewItemBook() doesn't reject null password");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "  123 ", "Alice’s Adventures in Wonderland", 3,
+				"Great book!", "5.4",
+				"reviewItemBook() doesn't reject that don't contain at least 4 characters (not taking into account leading or trailing blanks)");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "1234", null, 3, "Great book!", "5.5",
+				"reviewItemBook() doesn't reject null title");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "1234", "    ", 3, "Great book!", "5.6",
+				"reviewItemBook() doesn't reject only space title");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", -1,
+				"Great book!", "5.7", "reviewItemBook() doesn't reject a negative mark");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", 6,
+				"Great book!", "5.8", "reviewItemBook() doesn't reject a mark greater than 5");
+
+		nbTests++;
+		nbErrors += reviewItemBookBadEntryTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", 3, null,
+				"5.9", "reviewItemBook() doesn't reject null comments");
+
+		// Test n°6
+
+		nbTests++;
+		nbErrors += reviewItemBookNotMemberExceptionTest(sn, "Erwan", "1234", "Alice’s Adventures in Wonderland", 3,
+				"Great Book!", "6.1", "reviewItemBook() doesn't reject a bad login");
+
+		nbTests++;
+		nbErrors += reviewItemBookNotMemberExceptionTest(sn, "esevellec", "12345", "Alice’s Adventures in Wonderland",
+				3, "Great Book!", "6.2", "reviewItemBook() doesn't reject a bad password");
+
+		nbTests++;
+		nbErrors += reviewItemBookNotItemExceptionTest(sn, "esevellec", "1234", "Lord Of The Rings", 3, "Great Book!",
+				"6.3", "reviewItemBook() doesn't reject a non existing book title");
+
+		nbTests++;
+		nbErrors += reviewItemBookOKTest(sn, "esevellec", "1234", "Alice’s Adventures in Wonderland", 0, "Great Book!",
+				0, "6.4");
+
+		nbTests++;
+		nbErrors += reviewItemBookOKTest(sn, "kthezelais", "1234", "Alice’s Adventures in Wonderland", 5, "Great Book!",
+				(float) 2.5, "6.5");
+
+		nbTests++;
+		if (nbFilms != sn.nbFilms()) {
+			System.out.println("Error : the number of films was unexepectedly changed by addItemBook()");
+			nbErrors++;
+		}
+
+		nbTests++;
+		if (nbMembers != sn.nbMembers()) {
+			System.out.println("Error : the number of members was unexepectedly chaged by addItemBook()");
+		}
+
+		nbTests++;
+		if (nbBooks != sn.nbBooks()) {
+			System.out.println("Error : the number of books was unexepectedly chaged by review ItemBook()");
+		}
+
+		// Display final state of 'sn'
+		System.out.println("Final state of the social network : " + sn);
+
+		try {
+			TestReport tr = new TestReport(nbTests, nbErrors);
+			System.out.println("ReviewItemBook : " + tr);
+			return tr;
+		} catch (NotTestReportException e) {
+			System.out.println("Unexpected error in ReviewItemBookTest test code - Can't return valuable test results");
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
+		test();
+	}
+}
