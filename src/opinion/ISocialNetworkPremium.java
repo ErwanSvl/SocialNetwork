6f588106c98c71099baed30160571ff7a6dc1959
Erwan SEVELLEC <erwan.sevellec@imt-atlantique.net>
2018-05-28 11:48:43 +0200

    correct some details, diagramme and javadoc

--- src/opinion/ISocialNetworkPremium.java
+++ src/opinion/ISocialNetworkPremium.java
@@ -12,24 +12,31 @@ import exceptions.NotReviewException;
 import opinion.ISocialNetwork;
 
 public interface ISocialNetworkPremium extends ISocialNetwork {
-	
+
 	public enum Itemtype {
 		BOOK, FILM
 	}
 
 	/**
+	 * Add a opinion on an existing review
 	 * 
 	 * @param login
+	 *            the login of the author of the opinion
 	 * @param password
-	 * @param reviewAuthor TODO
+	 *            the password of the author of the opinion
+	 * @param reviewAuthor
+	 *            the login of the author of the review
 	 * @param title
+	 *            the title of the item reviewed
+	 * @param itemtype
+	 *            the type of the item (book or film)
 	 * @param mark
-	 * @return
+	 *            the mark of the opinion
 	 * @throws BadEntryException
 	 * @throws NotMemberException
 	 * @throws NotItemException
 	 * @throws NotReviewException
 	 */
-	public void reviewOpinion(String login, String password, String reviewAuthor, String title, Itemtype itemtype, float mark)
-			throws BadEntryException, NotMemberException, NotItemException, NotReviewException;
+	public void reviewOpinion(String login, String password, String reviewAuthor, String title, Itemtype itemtype,
+			float mark) throws BadEntryException, NotMemberException, NotItemException, NotReviewException;
 }
